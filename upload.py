a05975fb90f549a34c4222f3e62e4675427b2b72
Mikhail Sapozhnikov <MikeS@new-host-3.home>
2012-03-19 09:14:09 -0400

    Fixed external dir read issue, upload works

--- upload.py
+++ upload.py
@@ -1,16 +1,18 @@
 import soundcloud
 import re
+import os
 
-dir = '~/Music/'
+dir = '~/Music/' #set this to your desired soundcloud dir
 
 def upload_track( client, newTrack ):
 	print dir + newTrack
 	track = client.post('/tracks', track={
     		'title': get_title( newTrack ) ,
     		'sharing': 'private',
-    		'asset_data': open(dir + newTrack, 'rb')
+    		'asset_data': open(os.path.expanduser(dir + newTrack), 'rb')
 	})
 	print track.title + ' has been posted!'
+	print 'The secret URL is ' + track.permalink_url + '/' + track.secret_token
 
 def get_title ( newTrack ):
 	exp = re.compile('\A[\w-]*')
