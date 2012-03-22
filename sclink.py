f198ebc9c69a0ee55b5c3b4cd5940d9b81d93457
Mikhail Sapozhnikov <bulletblue@gmail.com>
2012-03-21 23:11:01 -0400

    working on observer module

--- sclink.py
+++ sclink.py
@@ -1,9 +1,19 @@
 import oauth_me
 import upload
+import os
+from fsevents import Observer,Stream
 
 client = oauth_me.get_auth()
 
-upload.upload_track( client, 'Pirates.mp3' )
+#upload.upload_track( client, 'Pirates.mp3' )
+
+def test_event( event ):
+	 print "Mask: %s, Cookie: %s, Name: %s" % (event.mask, event.cookie, event.name)
+
+obs = Observer()
+obs.start()
+
+stream = Stream( test_event, 'test', 
 
 
 #Poll directory for new music
